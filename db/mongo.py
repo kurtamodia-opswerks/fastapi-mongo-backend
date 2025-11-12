f170a9fd13ab4c96919639b3d2904836682ec5cf
kurtamodia-opswerks <km.amodia@opswerks.com>
2025-11-12 11:19:15 +0800

    chore: hide secrets

--- db/mongo.py
+++ db/mongo.py
@@ -1,7 +1,35 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+import os
+from pymongo.errors import ConnectionFailure
+import urllib.parse
 
-uri = "mongodb+srv://kurtmatthewamodia_db_user:Kmgwapo1*031300@vizly.srn8jql.mongodb.net/?appName=Vizly"
-client = MongoClient(uri, server_api=ServerApi("1"))
+# Load environment variables
+load_dotenv()
+
+username = os.getenv("MONGO_DB_USERNAME")
+password = os.getenv("MONGO_DB_PASSWORD")
+url = os.getenv("MONGO_DB_URL")
+
+# Encode password in case it contains special characters like * or @
+encoded_password = urllib.parse.quote_plus(password)
+
+uri = f"mongodb+srv://{username}:{encoded_password}@{url}"
+
+try:
+    client = MongoClient(uri, server_api=ServerApi("1"))
+    
+    client.admin.command("ping")
+    print("Successfully connected to MongoDB!")
+    
+    db = client["vizlydb"]
+    print("📘 Database selected:", db.name)
+
+except ConnectionFailure as e:
+    print("Could not connect to MongoDB:", e)
+except Exception as e:
+    print("An error occurred:", e)
+
 
 db = client["vizlydb"]
